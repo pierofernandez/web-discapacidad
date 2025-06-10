661ae2faebc5a683993e92a95b2234cd08c0fb02
pierofernandez <pierofernandezz48@gmail.com>
2025-06-10 01:30:55 -0500

    agregué el botón abandonar en el Home de discapacidad visual

--- src/pages/HomePageDV.jsx
+++ src/pages/HomePageDV.jsx
@@ -1,8 +1,24 @@
+import { FiLogOut } from "react-icons/fi";
+
 export const HomePageDV = () => {
     return (
         <div className="min-h-screen bg-[url('/img/discapacidad/fondoupn.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center p-6 space-y-6">
-            <img src="img/discapacidad/logoblanco.png" alt="logoupn" />
-
+            {/* Grid para logo centrado y botón abandonar a la derecha */}
+            <div className="w-full max-w-6xl grid grid-cols-3 items-center">
+                <div></div> {/* Columna vacía */}
+                <div className="flex justify-center">
+                    <img src="img/discapacidad/logoblanco.png" alt="logoupn" className="h-30" />
+                </div>
+                <div className="flex justify-end pr-4">
+                    <a
+                        href="/"
+                        className="text-white text-lg flex items-center gap-2 hover:text-red-400 transition"
+                    >
+                        <FiLogOut className="text-2xl" />
+                        Abandonar
+                    </a>
+                </div>
+            </div>
             {/* Fila 1 */}
             <div className="flex flex-row flex-wrap gap-6 w-full max-w-6xl justify-center">
                 <a href="/artedv" className="relative w-[35%] h-52 rounded-2xl overflow-hidden shadow-lg">
